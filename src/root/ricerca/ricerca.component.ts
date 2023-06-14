91c6606a9ab272d24e0191e31bb07bdf504728f8
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-14 11:55:12 +0200

    3 files modified, 3 added

---
+++ src/root/ricerca/ricerca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ricerca',
+  templateUrl: './ricerca.component.html',
+  styleUrls: ['./ricerca.component.css']
+})
+export class RicercaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/root/root.module.ts
+++ src/root/root.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RicercaComponent } from './ricerca/ricerca.component';
 import { RootComponent } from './root.component';
 
 @NgModule({
   declarations: [
-    RootComponent
+    RootComponent,
+    RicercaComponent
   ],
   imports: [
     CommonModule,
