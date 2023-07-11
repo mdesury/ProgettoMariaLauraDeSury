e091d8762b6e481cc92d1ef2c99d6358f7b91719
mdesury <127192048+mdesury@users.noreply.github.com>
2023-07-11 11:28:03 +0200

    2 files modified

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Archivio } from './../Objects/Archivio';
-import {Service} from "./../service.service";
+import { Service } from "./../service.service";
 
 @Component({
   selector: 'app-ricerca',
@@ -10,16 +10,25 @@ import {Service} from "./../service.service";
 export class RicercaComponent implements OnInit {
 
   archivio = new Archivio(this.servizio);
+  risultatiRicerca: Array<any> = []; // Variabile per memorizzare i risultati della ricerca
+  ricerca: string = '';
   
-
   ricercaLibro() {
-    let chiave = document.getElementById('trovaLibro') as HTMLInputElement;
-    
-
-    // Implementazione del metodo trovaLibro()
-    console.log(this.archivio.ricercaLibro(chiave.value));
+    let chiave = (document.getElementById('trovaLibro') as HTMLInputElement).value;
+  
+    if (chiave.trim() === '') {
+      this.risultatiRicerca = []; // Barra di ricerca vuota, assegna un array vuoto come risultato
+    } else {
+      this.risultatiRicerca = this.archivio.ricercaLibro(chiave);
+  
+      if (this.risultatiRicerca.length === 0) {
+        this.ricerca = 'Nessun libro trovato'; 
+      }
+    }
+  
     this.servizio.set(JSON.stringify(this.archivio.lista)).subscribe();
   }
+  
 
   constructor(private servizio: Service) {}
 
@@ -27,3 +36,4 @@ export class RicercaComponent implements OnInit {
 
   }
 }
+
