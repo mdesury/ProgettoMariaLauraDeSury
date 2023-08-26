68670fba672b041cb4eb9ce2180b16c2275e79d6
mdesury <127192048+mdesury@users.noreply.github.com>
2023-08-26 14:30:21 +0200

    2 files modified

--- src/root/prestito/prestito.component.ts
+++ src/root/prestito/prestito.component.ts
@@ -1,39 +1,47 @@
 import { Component, Input } from '@angular/core';
-import { Service } from "./../service.service";
-import { Libro } from './../Objects/Libro'; 
+import { Service } from './../service.service';
+import { Libro } from './../Objects/Libro';
 import { Archivio } from './../Objects/Archivio';
 
 @Component({
   selector: 'app-prestito',
   templateUrl: './prestito.component.html',
-  styleUrls: ['./prestito.component.css']
+  styleUrls: ['./prestito.component.css'],
 })
-
 export class PrestitoComponent {
   @Input() libroInPrestito: Libro | undefined;
   personaInPrestito: string = '';
   archivio: Archivio;
   codice: string = '';
+  errore: string = '';
 
   constructor(private servizio: Service) {
     this.archivio = new Archivio(this.servizio);
-    if(this.libroInPrestito) {
-      this.codice = this.archivio.trovaLibro(this.libroInPrestito.codice).codice;
+
+    if (this.libroInPrestito) {
+      this.codice = this.archivio.trovaLibro(
+        this.libroInPrestito.codice
+      ).codice;
+      this.personaInPrestito = this.archivio.trovaLibro(
+        this.libroInPrestito.codice
+      ).personaInPrestito;
     }
   }
 
   prendiInPrestito() {
-      this.archivio.prendiInPrestito(this.codice, this.personaInPrestito);
-      this.servizio.set(JSON.stringify(this.archivio.lista)).subscribe();
+    if (!this.personaInPrestito) {
+      this.errore = 'Devi compilare tutti i campi.';
+    }
+    this.archivio.prendiInPrestito(this.codice, this.personaInPrestito);
+    this.servizio.set(JSON.stringify(this.archivio.lista)).subscribe();
   }
 
   restituisciLibro() {
-      this.archivio.restituisci(this.codice);
-      this.servizio.set(JSON.stringify(this.archivio.lista)).subscribe();
+    this.archivio.restituisci(this.codice);
+    this.servizio.set(JSON.stringify(this.archivio.lista)).subscribe();
   }
 
   prendiPersonaInPrestito() {
-      return this.archivio.trovaLibro(this.codice).personaInPrestito;
+    return this.archivio.trovaLibro(this.codice).personaInPrestito;
   }
-
 }
