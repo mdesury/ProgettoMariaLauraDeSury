a55b91628e19cc62815907445b6f3f65a4c0acf5
mdesury <127192048+mdesury@users.noreply.github.com>
2023-09-05 18:37:29 +0200

    1 file modified

--- src/root/aggiungi/aggiungi.component.ts
+++ src/root/aggiungi/aggiungi.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Archivio } from './../Oggetti/Archivio';
 import { Service } from './../service.service';
 
